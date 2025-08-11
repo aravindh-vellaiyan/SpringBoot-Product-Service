86e907e78ed3d74ac0808ced3adca613ed65ee52
Aravindh V <aravindh.vellaiyan@gmail.com>
2025-08-11 10:55:32 +0530

    [Bugfix]Missed Getters and Setters for Exception DTO

--- ProductService/src/main/java/com/productservice/controllers/ProductController.java
+++ ProductService/src/main/java/com/productservice/controllers/ProductController.java
@@ -7,6 +7,7 @@ import com.productservice.models.Product;
 import com.productservice.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -40,7 +41,9 @@ public class ProductController {
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
-    private ExceptionDTO handlePNFException(ProductNotFoundException exception){
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+//    @ResponseBody
+    public ExceptionDTO handlePNFException(ProductNotFoundException exception){
         return new ExceptionDTO(exception.getMessage(), "Failure");
     }
 
--- ProductService/src/main/java/com/productservice/dtos/ExceptionDTO.java
+++ ProductService/src/main/java/com/productservice/dtos/ExceptionDTO.java
@@ -1,8 +1,10 @@
 package com.productservice.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @AllArgsConstructor
+@Data
 public class ExceptionDTO {
     private String message;
     private String status;
