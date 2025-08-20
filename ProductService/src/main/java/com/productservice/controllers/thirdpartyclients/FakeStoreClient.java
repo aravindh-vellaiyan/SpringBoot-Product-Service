67fbfebc43dea1d0d3762d8f32d5f784cd215e3d
Aravindh V <aravindh.vellaiyan@gmail.com>
2025-08-20 09:15:33 +0530

    [Enhancement]Moved FakeStore APIs from dedicated Client

---
+++ ProductService/src/main/java/com/productservice/controllers/thirdpartyclients/FakeStoreClient.java
@@ -0,0 +1,64 @@
+package com.productservice.controllers.thirdpartyclients;
+
+import com.productservice.dtos.FakeStoreProductDTO;
+import com.productservice.exceptions.ProductException;
+import com.productservice.exceptions.ProductNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+@Component
+public class FakeStoreClient {
+
+    private final String url = "https://fakestoreapi.com";
+
+    private final RestTemplateBuilder rtBuilder;
+
+    @Autowired
+    public FakeStoreClient(RestTemplateBuilder builder){
+        this.rtBuilder = builder;
+    }
+
+    public List<FakeStoreProductDTO> getAllProducts() throws ProductNotFoundException {
+        RestTemplate template = rtBuilder.build();
+        ResponseEntity<FakeStoreProductDTO[]> productsArr = template.getForEntity(url + "/products", FakeStoreProductDTO[].class);
+        if(productsArr.getBody() == null){
+            throw new ProductNotFoundException("Products not available.!");
+        }
+        return new LinkedList<>(Arrays.asList(productsArr.getBody()));
+    }
+
+    public FakeStoreProductDTO getProductById(Long id) throws ProductNotFoundException {
+        RestTemplate restTemplate = rtBuilder.build();
+        ResponseEntity<FakeStoreProductDTO> responseEntity = restTemplate.getForEntity(url + "/products/" + id, FakeStoreProductDTO.class);
+        if(responseEntity.getBody() == null){
+            throw new ProductNotFoundException("Product not found for the Id " + id);
+        }
+        return responseEntity.getBody();
+    }
+
+    public FakeStoreProductDTO deleteProductById(Long id) {
+        RestTemplate restTemplate = rtBuilder.build();
+        ResponseEntity<FakeStoreProductDTO> responseEntity = restTemplate.exchange(url + "/products/{id}", HttpMethod.DELETE, null, FakeStoreProductDTO.class, id);
+        if(responseEntity.getBody() == null){
+            throw new ProductException("Unable to delete the product for the Id " + id);
+        }
+        return responseEntity.getBody();
+    }
+
+    public FakeStoreProductDTO addProduct(FakeStoreProductDTO product)  throws ProductException {
+        RestTemplate restTemplate = rtBuilder.build();
+        ResponseEntity<FakeStoreProductDTO> responseEntity = restTemplate.postForEntity(url + "/products", product, FakeStoreProductDTO.class);
+        if(responseEntity.getBody() == null){
+            throw new ProductException("Unable to add the product");
+        }
+        return responseEntity.getBody();
+    }
+}
--- ProductService/src/main/java/com/productservice/services/FakeStoreServiceImpl.java
+++ ProductService/src/main/java/com/productservice/services/FakeStoreServiceImpl.java
@@ -1,16 +1,13 @@
 package com.productservice.services;
 
+import com.productservice.controllers.thirdpartyclients.FakeStoreClient;
 import com.productservice.dtos.FakeStoreProductDTO;
 import com.productservice.exceptions.ProductException;
 import com.productservice.exceptions.ProductNotFoundException;
 import com.productservice.models.Category;
 import com.productservice.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -18,24 +15,18 @@ import java.util.List;
 @Service("FakeStoreImpl")
 public class FakeStoreServiceImpl implements ProductService {
 
-    private final String url = "https://fakestoreapi.com";
-
-    private final RestTemplateBuilder rtBuilder;
+    private  final FakeStoreClient client;
 
     @Autowired
-    public FakeStoreServiceImpl(RestTemplateBuilder builder){
-        this.rtBuilder = builder;
+    public FakeStoreServiceImpl(FakeStoreClient client){
+        this.client = client;
     }
 
     @Override
     public List<Product> getAllProducts() throws ProductNotFoundException {
-        RestTemplate template = rtBuilder.build();
-        ResponseEntity<FakeStoreProductDTO[]> productsArr = template.getForEntity(url + "/products", FakeStoreProductDTO[].class);
-        if(productsArr.getBody() == null){
-            throw new ProductNotFoundException("Products not available.!");
-        }
+        List<FakeStoreProductDTO> fakeStoreProductDTOS = client.getAllProducts();
         List<Product> products = new LinkedList<>();
-        for(FakeStoreProductDTO fakeStoreProductDTO : productsArr.getBody()){
+        for(FakeStoreProductDTO fakeStoreProductDTO : fakeStoreProductDTOS){
             products.add(getProductObjectFromDTO(fakeStoreProductDTO));
         }
         return products;
@@ -43,32 +34,17 @@ public class FakeStoreServiceImpl implements ProductService {
 
     @Override
     public Product getProductById(Long id) throws ProductNotFoundException {
-        RestTemplate restTemplate = rtBuilder.build();
-        ResponseEntity<FakeStoreProductDTO> responseEntity = restTemplate.getForEntity(url + "/products/" + id, FakeStoreProductDTO.class);
-        if(responseEntity.getBody() == null){
-            throw new ProductNotFoundException("Product not found for the Id " + id);
-        }
-        return getProductObjectFromDTO(responseEntity.getBody());
+        return getProductObjectFromDTO(client.getProductById(id));
     }
 
     @Override
     public Product deleteProductById(Long id) {
-        RestTemplate restTemplate = rtBuilder.build();
-        ResponseEntity<FakeStoreProductDTO> responseEntity = restTemplate.exchange(url + "/products/{id}", HttpMethod.DELETE, null, FakeStoreProductDTO.class, id);
-        if(responseEntity.getBody() == null){
-            throw new ProductException("Unable to delete the product for the Id " + id);
-        }
-        return getProductObjectFromDTO(responseEntity.getBody());
+        return getProductObjectFromDTO(client.deleteProductById(id));
     }
 
     @Override
     public Product addProduct(Product product)  throws ProductException {
-        RestTemplate restTemplate = rtBuilder.build();
-        ResponseEntity<FakeStoreProductDTO> responseEntity = restTemplate.postForEntity(url + "/products", getDTOFromProduct(product), FakeStoreProductDTO.class);
-        if(responseEntity.getBody() == null){
-            throw new ProductException("Unable to add the product");
-        }
-        return getProductObjectFromDTO(responseEntity.getBody());
+        return getProductObjectFromDTO(client.addProduct(getDTOFromProduct(product)));
     }
 
     @Override
