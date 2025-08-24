b44af1a69569166fb10b3f9019c208de484e6506
Aravindh V <aravindh-9638@aravindh-9638.csez.zohocorpin.com>
2025-08-24 14:05:01 +0530

    Table auto creation using Hibernate

--- ProductService/src/main/java/com/productservice/models/BaseModel.java
+++ ProductService/src/main/java/com/productservice/models/BaseModel.java
@@ -1,14 +1,19 @@
 package com.productservice.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @MappedSuperclass
 @Getter
 @Setter
 public class BaseModel {
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
-    Long id;
+    UUID id;
 }
