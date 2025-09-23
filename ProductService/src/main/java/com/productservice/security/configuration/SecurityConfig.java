144a1432a6e3e81b7df7ecce5d444ad67326b54d
Aravindh V <aravindh.vellaiyan@gmail.com>
2025-09-23 23:25:51 +0530

    Authentication Layer to ProductService

--- ProductService/src/main/java/com/productservice/security/configuration/SecurityConfig.java
+++ ProductService/src/main/java/com/productservice/security/configuration/SecurityConfig.java
@@ -1,4 +1,24 @@
 package com.productservice.security.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.authorizeHttpRequests((authorize) -> {
+//            authorize.anyRequest().authenticated();
+//        }).formLogin(Customizer.withDefaults());
+        httpSecurity
+                .authorizeHttpRequests((authorize) -> {
+                    authorize
+                            .anyRequest().authenticated();
+                })
+                .cors().disable()
+                .csrf().disable()
+        return httpSecurity.build();
+    }
 }
